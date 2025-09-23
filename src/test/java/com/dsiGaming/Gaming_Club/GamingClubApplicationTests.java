d118fbd2df75c561a6c7b9aa9c94f6c7b15a52fa
Ushasp <ushasp687@gmail.com>
2025-09-23 14:22:09 +0530

    initial commit

---
+++ src/test/java/com/dsiGaming/Gaming_Club/GamingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsiGaming.Gaming_Club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
